6df4100094ec6095067b80112638f00882e29f0b
RachevNikita <Prib-201_831838@volsu.ru>
2021-05-22 20:06:41 +0300

    Передача файлов на сервер

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "sdt.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int number_one;
+    int number_two;
+    int c;
+    cout << "Введите два целых числа: ";
+
+    cin >> number_one;
+     cin >> number_two;
+
+
+    cout << "number_one: " << number_one << endl;
+     cout << "number_two: " << number_two << endl;
+
+	c=number_one+number_two;
+    cout << "Сумма двух чисел: " << c << endl;
+
+
+      
+
+
+
+
+
+    getchar();
+    getchar();
+    return 0;
+}
